e35cf2f7561774ae8d3a4c2862671914bce586ed
konrad <konrad@tyk.io>
2022-10-19 17:48:13 +0200

    Delete test_create_bank_account.py

--- tests/test_create_bank_account.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from source import Konto
-
-class TestCreateBankAccount(unittest.TestCase):
-
-    def test_create_account(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
