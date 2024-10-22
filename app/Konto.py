00fe49afa738e7966f1cb465b035dc4c675ce8f3
konrad <konrad@tyk.io>
2024-10-22 12:18:48 +0200

    lab3

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,13 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.saldo = 0
+    saldo = 0
+    express_transfer_fee = 0
+    def incoming_transfer(self, kwota):
+        self.saldo += kwota
+
+    def outgoing_transfer(self, kwota):
+        if self.saldo >= kwota:
+            self.saldo -= kwota
+
+    def outgoing_express_transfer(self, kwota):
+        if self.saldo >= kwota:
+            self.saldo -= kwota + self.express_transfer_fee
