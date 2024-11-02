fdb02d94515ecb96566c03b755cbb708e6e6f2f7
konrad <konrad@tyk.io>
2024-11-02 12:20:32 +0100

    po rafaktorze

--- app/PersonalAccount.py
+++ app/PersonalAccount.py
@@ -22,16 +22,17 @@ class PersonalAccount(Konto):
         return False
     
     def take_loan(self, kwota):
-        if len(self.history) < 3:
-            return 
-        if sum(self.history[-5:]) > kwota:
+        if self.is_last_3_transactions_positive() or self.is_sum_of_last_5_transactions_greater_than_loan(kwota):
             self.saldo += kwota
             self.history.append(kwota)
-        if self.history[-1] > 0 and self.history[-2] > 0 and self.history[-3] > 0:
-            self.saldo += kwota
-            self.history.append(kwota)
-        
 
+    def is_last_3_transactions_positive(self):
+        if len(self.history) < 3:
+            return False
+        return self.history[-1] > 0 and self.history[-2] > 0 and self.history[-3] > 0
+        
+    def is_sum_of_last_5_transactions_greater_than_loan(self, loan):
+        return sum(self.history[-5:]) > loan
     
 
     
--- app/tests/test_loan.py
+++ app/tests/test_loan.py
@@ -1,4 +1,5 @@
 import unittest
+from parameterized import parameterized
 
 from ..PersonalAccount import PersonalAccount
 
@@ -7,32 +8,19 @@ class TestLoan(unittest.TestCase):
     nazwisko = "Januszewski"
     pesel = "12345678901"
 
-    def test_loan_3_last_incoming(self):
-        konto = PersonalAccount(self.imie, self.nazwisko, self.pesel)
-        konto.history = [-100, 200, -33, 10, 200, 50]
-        konto.take_loan(1000)
-        self.assertEqual(konto.saldo, 1000, "Saldo nie zostało zwiększone!")
+    def setUp(self) -> None:
+        self.konto = PersonalAccount(self.imie, self.nazwisko, self.pesel)
 
-    def test_loan_5_transactions(self):
-        konto = PersonalAccount(self.imie, self.nazwisko, self.pesel)
-        konto.history = [-100, 200, 5000, 1, -2, -3]
-        konto.take_loan(1000)
-        self.assertEqual(konto.saldo, 1000, "Saldo nie zostało zwiększone!")
-
-    def test_loan_negative(self):
-        konto = PersonalAccount(self.imie, self.nazwisko, self.pesel)
-        konto.history = [-100, 200, 500, 100, -1, 300]
-        konto.take_loan(10000)
-        self.assertEqual(konto.saldo, 0, "Saldo nie zostało zwiększone!")
-
-    def test_loan_2_positions_in_history(self):
-        konto = PersonalAccount(self.imie, self.nazwisko, self.pesel)
-        konto.history = [200, 500]
-        konto.take_loan(10000)
-        self.assertEqual(konto.saldo, 0, "Saldo nie zostało zwiększone!")
-    
-    def test_loan_2_positions_in_history_high_sum(self):
-        konto = PersonalAccount(self.imie, self.nazwisko, self.pesel)
-        konto.history = [-666, 200000, 500, -1, 200]
-        konto.take_loan(10000)
-        self.assertEqual(konto.saldo, 10000, "Saldo nie zostało zwiększone!")
+    @parameterized.expand([
+        ([-100, 200, -33, 10, 200, 50], 1000, 1000),
+        ([-100, 200, 5000, 1, -2, -3], 1000, 1000),
+        ([-100, 200, 5000, 1, -2, -3], 1000, 1000),
+        ([-100, 200, 500, 100, -1, 300], 10000, 0),
+        ([200, 500], 10000, 0),
+        ([-666, 200000, 500, -1, 200], 10000, 10000)
+    ])
+    def test_loan(self, history, loan, expected):
+        print(f"Test dla historii: {history}, pożyczki: {loan}, oczekiwane saldo: {expected}")
+        self.konto.history = history
+        self.konto.take_loan(loan)
+        self.assertEqual(self.konto.saldo, expected, "Saldo nie zostało zwiększone!")
